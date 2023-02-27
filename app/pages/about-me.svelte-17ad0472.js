b32bdcc09963f3acdb7400f622b790302342164c
mercvre <jordan_louis@outlook.com>
2023-02-27 15:53:28 +0100

    Updated my bio.

---
+++ app/pages/about-me.svelte-17ad0472.js
@@ -0,0 +1,25 @@
+import{S as H,i as W,s as Y,k as y,e as r,t as l,N as z,d as n,n as w,c,a as d,g as h,b as p,f as _,E as e,G as J}from"../chunks/vendor-b2f0db4e.js";function B(F){let u,m,q,x,t,b,E,j,f,C,G,g,L,A,v,M,S,o,P,s,U,D;return{c(){u=y(),m=r("h1"),q=l("Jordan Louis"),x=y(),t=r("div"),b=r("p"),E=l(`As a fast-learning multipotentialite with two Master's degrees in Design and Computer Science, I
+		bring a unique blend of aesthetic sensibility and technical expertise to my work which make me
+		an ideal candidate for a software engineering role.`),j=y(),f=r("p"),C=l(`I have experience developing pixel-perfect interfaces and a deep understanding of UI/UX design
+		principles. It is my belief that learning is a lifelong journey, it keeps me pushing beyond my
+		boundaries and my interdisciplinary approach allows me to deliver high-quality results that
+		exceed expectations.`),G=y(),g=r("p"),L=l(`I have a background in web development, using the latest technologies such as Svelte framework
+		and am experienced in C++. My academic projects in C++ range from developing interactive
+		applications using Qt framework to manipulating shaders with OpenGL.`),A=y(),v=r("p"),M=l(`I believe that innovation comes at the intersection of different fields and that quality is more
+		important than quantity. I bring a commitment to excellence and a willingness to explore new
+		environments and technologies to everything I do. My goal is to create impactful and unique
+		solutions that drive positive change.`),S=y(),o=r("p"),P=l(`If you're seeking a highly motivated and creative junior software engineer, I am excited about
+		the opportunity to work with like-minded individuals and create products that make a difference.
+		`),s=r("a"),U=l("Let's connect"),D=l(" and start a conversation about what we can achieve together."),this.h()},l(a){z('[data-svelte="svelte-1d8u1ym"]',document.head).forEach(n),u=w(a),m=c(a,"H1",{});var X=d(m);q=h(X,"Jordan Louis"),X.forEach(n),x=w(a),t=c(a,"DIV",{class:!0});var i=d(t);b=c(i,"P",{});var N=d(b);E=h(N,`As a fast-learning multipotentialite with two Master's degrees in Design and Computer Science, I
+		bring a unique blend of aesthetic sensibility and technical expertise to my work which make me
+		an ideal candidate for a software engineering role.`),N.forEach(n),j=w(i),f=c(i,"P",{class:!0});var O=d(f);C=h(O,`I have experience developing pixel-perfect interfaces and a deep understanding of UI/UX design
+		principles. It is my belief that learning is a lifelong journey, it keeps me pushing beyond my
+		boundaries and my interdisciplinary approach allows me to deliver high-quality results that
+		exceed expectations.`),O.forEach(n),G=w(i),g=c(i,"P",{class:!0});var Q=d(g);L=h(Q,`I have a background in web development, using the latest technologies such as Svelte framework
+		and am experienced in C++. My academic projects in C++ range from developing interactive
+		applications using Qt framework to manipulating shaders with OpenGL.`),Q.forEach(n),A=w(i),v=c(i,"P",{class:!0});var V=d(v);M=h(V,`I believe that innovation comes at the intersection of different fields and that quality is more
+		important than quantity. I bring a commitment to excellence and a willingness to explore new
+		environments and technologies to everything I do. My goal is to create impactful and unique
+		solutions that drive positive change.`),V.forEach(n),S=w(i),o=c(i,"P",{class:!0});var I=d(o);P=h(I,`If you're seeking a highly motivated and creative junior software engineer, I am excited about
+		the opportunity to work with like-minded individuals and create products that make a difference.
+		`),s=c(I,"A",{href:!0,"data-contact":!0,onclick:!0});var Z=d(s);U=h(Z,"Let's connect"),Z.forEach(n),D=h(I," and start a conversation about what we can achieve together."),I.forEach(n),i.forEach(n),this.h()},h(){document.title="Jordan Louis - About me",p(f,"class","mt-16"),p(g,"class","mt-16"),p(v,"class","mt-16"),p(s,"href","#"),p(s,"data-contact","aGVsbG8uam9yZGFubG91aXNAZ21haWwuY29t"),p(s,"onclick","this.href = 'mailto:' + atob(this.dataset.contact)"),p(o,"class","mt-16"),p(t,"class","max-width mt-16 mb-48")},m(a,k){_(a,u,k),_(a,m,k),e(m,q),_(a,x,k),_(a,t,k),e(t,b),e(b,E),e(t,j),e(t,f),e(f,C),e(t,G),e(t,g),e(g,L),e(t,A),e(t,v),e(v,M),e(t,S),e(t,o),e(o,P),e(o,s),e(s,U),e(o,D)},p:J,i:J,o:J,d(a){a&&n(u),a&&n(m),a&&n(x),a&&n(t)}}}class R extends H{constructor(u){super();W(this,u,null,B,Y,{})}}export{R as default};
